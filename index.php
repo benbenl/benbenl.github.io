1ea883efbaada16a61d5b8abe03ec7b09dd887b8
benbenl <evekana@gmail.com>
2016-05-17 14:46:22 -0700

    update index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Benben Li</title>
+
+    <style>
+        body {
+            background: #fff url('img/benbenli-bg.svg') no-repeat center center; 
+            background-size: cover;
+            width: 100vw;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
